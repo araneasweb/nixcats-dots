5ce2f81bdbf7697ef913795689e3f52cd9d2ef20
aranea <anahita@golshani.ca>
2026-03-04 22:34:22 -0800

    unidentified global `vim` fix

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -1,15 +1,22 @@
 return {
 	settings = {
 		Lua = {
+			runtime = {
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				globals = { "vim", "nixCats" },
+			},
+			workspace = {
+				checkThirdParty = false,
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
 			completion = {
 				callSnippet = "Replace",
 			},
 			hint = {
 				enable = true,
 				setType = true,
-			},
-			diagnostics = {
-				globals = { "nixCats" },
 			},
 		},
 	},
